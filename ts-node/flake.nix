1fa78d2b7f3246229e7a1929a8d5dbb7002c01bf
Lord-Valen <lord_valen@protonmail.com>
2022-07-17 23:19:14 -0400

    :brain: `redesign` rewrite ts-node

--- ts-node/flake.nix
+++ ts-node/flake.nix
@@ -16,52 +16,26 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        packageName = throw "You done forgot to put in the dang package name!";
+        inherit (import ./default.nix { inherit pkgs; }) package shell;
+        packageName = throw
+          "You done forgot to put in the dang package name! Don't forget to put it in package.json!";
       in {
         packages = {
           default = self.packages.${system}.${packageName};
+          ${packageName} = package;
+        };
 
-          ${packageName} = pkgs.stdenv.mkDerivation {
+        apps = {
+          default = self.apps.${system}.${packageName};
+          ${packageName} = flake-utils.lib.mkApp {
             name = packageName;
-            src = self;
-            buildInputs = with pkgs; [ nodejs ];
-
-            buildPhase = let
-              nodeDeps = (pkgs.callPackage ./default.nix { }).nodeDependencies;
-            in ''
-              rm -rf node_modules
-              ln -s ${nodeDeps}/lib/node_modules node_modules
-              export PATH="${nodeDeps}/bin:$PATH"
-
-              npm run build;
-            '';
-
-            installPhase = ''
-              mkdir -p $out
-              cp -r dist $out
-            '';
+            drv = package;
           };
         };
 
-        devShells.default = let
-          nodeEnv = pkgs.callPackage ./node-env.nix { };
-          nodePkgs = pkgs.callPackage ./node-packages.nix {
-            globalBuildInputs = with pkgs; [ node2nix ];
-            inherit nodeEnv;
-          };
-        in nodePkgs.shell.overrideAttrs (oldAttrs: {
+        devShells.default = shell.overrideAttrs (oldAttrs: {
           shellHook = oldAttrs.shellHook + ''
             export PS1='dev:\w > \[$(tput sgr0)\]'
-
-            # Certain tools (including jest) will not work properly without this.
-            rm -rf node_modules
-            ln -s $NODE_PATH node_modules
-
-            abort() {
-              rm -rf node_modules
-            }
-
-            trap abort EXIT
           '';
         });
       });
