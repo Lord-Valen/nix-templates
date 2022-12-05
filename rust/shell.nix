0c4d5aef28fcadda71d2d792099c1f35ab77b610
Lord-Valen <lord_valen@protonmail.com>
2022-12-05 18:25:55 -0500

    :gem: `style` Run treefmt

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,11 +1,15 @@
-(import
-  (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+(import (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
     in
-    fetchTarball {
-      url =
-        "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  { src = builtins.path { path = ./.; name = "projectRoot"; }; }).shellNix
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+  ) {
+    src = builtins.path {
+      path = ./.;
+      name = "projectRoot";
+    };
+  })
+.shellNix
