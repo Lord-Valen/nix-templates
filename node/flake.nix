89fd9c5b73ee43341d019235d2091034566e1239
Lord-Valen <lord_valen@protonmail.com>
2022-12-05 17:38:50 -0500

    :rocket: `feat` Use flake-utils

--- node/flake.nix
+++ node/flake.nix
@@ -8,6 +8,7 @@
     };
 
     nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
 
     dream2nix = {
       url = "github:nix-community/dream2nix";
@@ -15,40 +16,46 @@
     };
   };
 
-  outputs = { dream2nix, nixpkgs, ... }:
-    let
-      systems =
-        [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs systems
-        (system: f system (nixpkgs.legacyPackages.${system}));
-      projectRoot = builtins.path {
-        path = ./.;
-        name = "projectRoot";
-      };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    dream2nix,
+    ...
+  }: let
+    projectRoot = builtins.path {
+      path = ./.;
+      name = "projectRoot";
+    };
 
-      d2n-flake = dream2nix.lib.makeFlakeOutputs {
-        inherit systems;
-        config.projectRoot = projectRoot;
-        source = projectRoot;
-        settings = [{ subsystemInfo.nodejs = 18; }];
-      };
-    in dream2nix.lib.dlib.mergeFlakes [
+    d2n-flake = dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
+      config.projectRoot = projectRoot;
+      source = projectRoot;
+      settings = [{subsystemInfo.nodejs = 18;}];
+    };
+  in
+    dream2nix.lib.dlib.mergeFlakes [
       d2n-flake
-      {
-        devShells = forAllSystems (system: pkgs: {
-          default = d2n-flake.devShells.${system}.default.overrideAttrs (old: {
-            buildInputs = old.buildInputs ++ [
-              # Formatters
-              pkgs.treefmt
-              pkgs.alejandra
-              pkgs.nodePackages.prettier
+      (flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          devShells = {
+            default = d2n-flake.devShells.${system}.default.overrideAttrs (old: {
+              buildInputs =
+                old.buildInputs
+                ++ [
+                  # Formatters
+                  pkgs.treefmt
+                  pkgs.alejandra
+                  pkgs.nodePackages.prettier
 
-              # Lint
-              pkgs.editorconfig-checker
-            ];
-          });
-        });
-      }
+                  # Lint
+                  pkgs.editorconfig-checker
+                ];
+            });
+          };
+        }
+      ))
     ];
 }
