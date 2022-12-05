fbbc13be4d18aaaa74a616345ca9d73900e808b2
Lord-Valen <lord_valen@protonmail.com>
2022-12-05 18:25:43 -0500

    :rocket: `feat` Add formatters

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,30 +1,53 @@
 {
   description = "Rust template, using dream2nix";
 
-  inputs =
-    {
-      flake-compat = {
-        url = "github:edolstra/flake-compat";
-        flake = false;
-      };
-
-      nixpkgs = {
-        url = "nixpkgs";
-      };
-
-      dream2nix = {
-        url = "github:nix-community/dream2nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
 
-  outputs = { dream2nix, nixpkgs, ... }:
-    let
-      projectRoot = builtins.path { path = ./.; name = "projectRoot"; };
-    in
-    dream2nix.lib.makeFlakeOutputs {
-      systems = [ "x86_64-linux" ];
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    dream2nix,
+    ...
+  }: let
+    projectRoot = builtins.path {
+      path = ./.;
+      name = "projectRoot";
+    };
+
+    d2nFlake = dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
       config.projectRoot = projectRoot;
       source = projectRoot;
     };
+  in
+    dream2nix.lib.dlib.mergeFlakes [
+      d2nFlake
+      (flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = d2nFlake.devShells.${system}.default.addConfig {
+            packages = [
+              pkgs.treefmt
+              pkgs.alejandra
+              pkgs.rustfmt
+              pkgs.editorconfig-checker
+            ];
+          };
+        };
+      }))
+    ];
 }
