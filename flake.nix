b17dc9cb43b87d74c5af27c7996896e4872313b4
Lord-Valen <lord_valen@protonmail.com>
2022-12-05 21:06:57 -0500

    :recycle: `env` Add pre-commit hook

--- flake.nix
+++ flake.nix
@@ -1,24 +1,67 @@
 {
   description = "My nix flake templates, using flake-compat";
 
-  outputs = { self }: {
-    templates =
-      let inherit (builtins) path; in
-      {
+  inputs.devshell = {
+    url = "github:numtide/devshell";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      templates = {
         haskell = {
-          path = path { path = ./haskell; name = "haskellTemplate"; };
+          path = builtins.path {
+            path = ./haskell;
+            name = "haskellTemplate";
+          };
           description = "Haskell template, using cabal2nix";
         };
 
         node = {
-          path = path { path = ./node; name = "tsNodeTemplate"; };
+          path = builtins.path {
+            path = ./node;
+            name = "tsNodeTemplate";
+          };
           description = "NodeJS TypeScript template, using dream2nix";
         };
 
         rust = {
-          path = path { path = ./rust; name = "rustTemplate"; };
+          path = builtins.path {
+            path = ./rust;
+            name = "rustTemplate";
+          };
           description = "Rust template, using dream2nix";
         };
       };
-  };
+
+      devShells.default = let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [devshell.overlay];
+        };
+      in
+        pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.extraModulesDir + "/git/hooks.nix")
+            (pkgs.devshell.importTOML (builtins.path {
+              path = ./devshell.toml;
+              name = "devshell";
+            }))
+          ];
+
+          git.hooks = {
+            enable = true;
+            pre-commit.text = builtins.readFile (builtins.path {
+              path = ./pre-commit.sh;
+              name = "pre-commit";
+            });
+          };
+        };
+    });
 }
